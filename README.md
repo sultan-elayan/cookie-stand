1d0e2be6f0cf514ad61a41833411b8eef832ef58
sultan-elayan <sultan.m.elayan@gmail.com>
2021-05-02 23:13:51 +0300

    updating

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # cookie-stand
+
+<!--i used adobe color to chose the colors in this website-->
+<!--i work with qusai alqasi on this project-->
+<!--we git some help from TAs-->
+<!--we look at barah repo and see what she use , but we dosent use any copy-paste-->
--- js/app.js
+++ js/app.js
@@ -1,72 +1,36 @@
 'use strict'
 
-let hours = ['6AM' , '7AM' , '8AM' , '9AM' , '10AM' , '11AM' , '12AM' , '1PM' , '2PM' , '3PM' , '4PM' , '5PM' , '6PM' , '7PM']
+
+let hours = ['6AM' , '7AM' , '8AM' , '9AM' , '10AM' , '11AM' , '12AM' , '1PM' , '2PM' , '3PM' , '4PM' , '5PM' , '5PM' , '6PM' , '7PM']
+ let avaregCookiesForCustomerPerHour = [];
 let Seattle = {
     name: 'Seattle',
     maxHourlyCustomers: 65,
     minHourlyCustomers: 23,
     averageCookiesPerCustomer: 6.3,
-    avaregCookesForCustmer: [],
     getCookies: function () {
         for (let i = 0; i < hours.length; i++) {
-
-            this.avaregcookesforcustmer.push(Math.ceil(getrandom(this.mincustmerhourly, this.maxcustmerhourly) * this.avaregcookes));
-
-
+            avaregCookiesForCustomerPerHour.push(Math.ceil(getRandom(this.minHourlyCustomers, this.maxHourlyCustomers) * this.averageCookiesPerCustomer));
         }
-
+    },
+    render : function () {
+        let list1 = document.getElementById('seatle');
+        let pharaghrap =document.createElement('p');
+        pharaghrap.textContent=this.name; 
+        list1.appendChild(pharaghrap);
+        let ulElement = document.createElement('ul');
+        list1.appendChild(ulElement);
+        for (let i =0 ;  i< hours.length; i++) {
+            let liElement = document.createElement('li');
+            ulElement.appendChild(liElement);
+            for (let j=0 ; j<avaregCookiesForCustomerPerHour.length; j++){
+            }
+           liElement.textContent = `${hours[i]}:${avaregCookiesForCustomerPerHour[i]} cookes`
+        }
     }
-
 }
-
-
-// function getRandomArbitrary(min, max) {
-    //     console.log(min)
-// console.log(max)
-// console.log(Math)
-//     return Math.random() * (max - min) + min;
-//   }
-
-
-
-// getRandomArbitrary(23,65);
-// let ans = (getRandomInt(23,65))
-
-
-
-
-
-
-
-
-// 'use strict';
-// let hour = ['6AM' , '7AM' , '8AM' , '9AM' , '10AM' , '11AM' , '12AM' , '1PM' , '2PM' , '3PM' , '4PM' , '5PM' , '6PM' , '7PM']
-//  let avaregcookesforcustmer = [];
-// let Seattle = {
-
-//     name: 'Seattle',
-//     maxcustmerhourly: 65,
-//     mincustmerhourly: 23,
-//     avaregcookes: 6.3,
-//     // avaregcookesforcustmer: [],
-//     getCookies: function () {
-//         for (let i = 0; i < hour.length; i++) {
-
-//             avaregcookesforcustmer.push(Math.ceil(getRandom(this.mincustmerhourly, this.maxcustmerhourly) * this.avaregcookes));
-
-
-//         }
-
-//     },
-
-//     render : function () {
-//         let listOne = document.getElementById('seatle');
-//         let par =document.createElement('p');
-//         par.textContent=this.name; 
-//         listOne.appendChild(par);
-//         let ulElemant = document.createElement('ul');
-//         listOne.appendChild(ulElemant);
-//         for (let i =0 ;  i< hour.length; i++) {
-//             let liElemant = document.createElement('li');
-//             ulElemant.appendChild(liElemant);
-//             // for (let j=0
+Seattle.getCookies();
+Seattle.render(); 
+function getRandom(min, max) {
+    return Math.ceil(Math.random() * (max - min + 1) + min);
+}
